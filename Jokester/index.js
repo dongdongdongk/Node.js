b17bff692786b61df67d3c000b9a85046682351f
dongdongdongk <dhk9309@gmail.com>
2023-12-18 03:26:48 +0900

    npm

--- Jokester/index.js
+++ Jokester/index.js
@@ -1,6 +1,13 @@
 const jokes = require("give-me-a-joke");
 const colors = require('colors');
+const cowsay = require('cowsay');
 // To get a random dad joke
 jokes.getRandomDadJoke (function(joke) {
     console.log(joke.rainbow);
 });
+
+console.log(cowsay.say({
+    test : "test",
+    e : "oO",
+    T : "U "
+}));
