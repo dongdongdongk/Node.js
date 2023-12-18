6162c8168e3117851d3075f8748d907740061138
dongdongdongk <dhk9309@gmail.com>
2023-12-19 01:58:46 +0900

    get,post

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -6,9 +6,27 @@ console.dir(app)
 
 //.use 들어오는 요청이 있다면 무조건 응답
 // app.use(() => {
-//     console.log("NEW REQUEST");
-// })
+    //     console.log("NEW REQUEST");
+    // })
+app.get('/',(req,res) => {
+        res.send("HOME")
+})
 
+app.get('/search', (req,res) => {
+    const { q } = req.query;
+    if(!q) {
+        res.send('쿼리가 없습니다.')
+    }
+    res.send(`<h1>쿼리 결과는 : ${q} </h1>`)
+})
+
+
+app.get('/r/:subreddit/:postId',(req,res) => {
+    const {subreddit, postId} = req.params;
+    res.send(`<h1>Id는${postId}매핑값은${subreddit}</h1>`);
+
+})
+    
 app.get('/cats',(req,res) => {
     res.send("GET CAT")
 })
@@ -21,9 +39,6 @@ app.post('postCat',(req,res) => {
     res.send("POST CAT")
 })
 
-app.get('/',(req,res) => {
-    res.send("HOME")
-})
 
 app.get('*',(req,res) => {
     res.send("지정된 경로가 없습니다.")
