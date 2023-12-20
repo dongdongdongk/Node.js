7838006d30f3b4a31fc008b16c9fc4b9a79da1f1
dongdongdongk <dhk9309@gmail.com>
2023-12-21 00:42:16 +0900

    shopping

--- React-App/React-App/src/ShoppingList.jsx
+++ React-App/React-App/src/ShoppingList.jsx
@@ -4,10 +4,10 @@ function ShoppingList({item}) {
     <ul>
         {item.map((i) => ( 
             <ShoppingListItem
-                key={i.id}
-                item={i.item}
-                quantity={i.quantity}
-                completed={i.completed}
+                key={i.id} {...i}
+                // item={i.item}
+                // quantity={i.quantity}
+                // completed={i.completed}
             />
             ))}
     </ul>
