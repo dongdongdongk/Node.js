249979051c34612a43021f0c49fc9fa6b987e5f3
dongdongdongk <dhk9309@gmail.com>
2023-12-21 18:25:38 +0900

    test

--- state-demo/src/Counter.jsx
+++ state-demo/src/Counter.jsx
@@ -1,8 +1,8 @@
-import { userState } from "react";
+import {useState} from "react";
 export default function Counter() {
-    const [count, setCount] = userState(0);
+    const [count, setCount] = useState(0);
     const incrementCount = () => {
-        setCount(count + 1);
+        setCount(c => c + 1);
     }
 
     return (
