3c7e6c58b06a2bc42cdbda17e857bbd68b57b2bf
dongdongdongk <dhk9309@gmail.com>
2023-12-27 02:17:37 +0900

    UI

--- material-ui-demo/src/App.jsx
+++ material-ui-demo/src/App.jsx
@@ -5,11 +5,13 @@ import AlarmIcon from '@mui/icons-material/Alarm';
 import IconButton from '@mui/material/IconButton';
 import RatingDemo from './RatingDemo';
 import FormDemo from './FormDemo';
+import NavBar from './Navbar';
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <FormDemo />
     </>
   )
--- material-ui-demo/src/FormDemo.jsx
+++ material-ui-demo/src/FormDemo.jsx
@@ -1,15 +1,24 @@
 import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 import { useState } from 'react';
 function FormDemo() {
     const [name,setName] = useState();
+    const [volume, setVolume] = useState(50);
     const updateName = (e) => {
         setName(e.target.value);
     }
+    const volumeChange = (e,newVal) => {
+        setVolume(newVal);
+    }
     return(
-        <div>
+        
+        <Box sx={{border:'1px solid red',p:6, width: 300, height: 300, margin:"0 auto"}}>
             <h1>Name is : {name}</h1>
             <TextField id="outlined-basic" label="Outlined" variant="outlined" placeholder='name' value={name} onChange={updateName} />
-        </div>
+            <h2>Volume : {volume}</h2>
+            <Slider aria-label="Volume" value={volume} onChange={volumeChange} />
+        </Box>
     )
 }
 
