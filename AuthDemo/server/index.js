d565ffec923317a5c9d776575ce26ed39a0bbc0f
dongdongdongk <dhk9309@gmail.com>
2024-01-14 04:40:00 +0900

    User

--- AuthDemo/server/index.js
+++ AuthDemo/server/index.js
@@ -37,43 +37,32 @@ db.once("open", () => { // 한 번만 실행되는 이벤트 리스너 등록 MongoDB에 성공적�
     console.log("Database connected");
 });
 
+
+
+
 // 사용자 등록 엔드포인트
 app.post('/register', async (req, res) => {
     // 클라이언트에서 전송된 사용자 정보를 받아옵니다.
     const { password, username } = req.body;
-    
-    // 받아온 비밀번호를 bcrypt를 사용해 해시값으로 변환합니다.
-    const hash = await bcrypt.hash(password, 12);
-    
-    // 새로운 사용자 객체를 생성하고 MongoDB에 저장합니다.
-    const user = new User({
-        username,
-        password: hash
-    })
-    await user.save();
+    const user = new User({username, password})
+    await user.save()
     // req.session.user_id = user._id; // 등록하고 바로 로그인 하려면 바로 발급
     
-    
     // 클라이언트에게 해시값을 응답으로 보냅니다.
-    res.send(hash);
+    res.send({success : true, user});
 })
 
 // 사용자 로그인 엔드포인트
 app.post('/login', async (req, res) => {
     // 클라이언트에서 전송된 사용자 정보를 받아옵니다.
     const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password)
     
-    // MongoDB에서 해당 사용자를 찾습니다.
-    const user = await User.findOne({ username }); // 사용자명이 고유하다고 가정하고 있기 때문에 findOne
-    
-    // 사용자가 존재하지 않거나 비밀번호가 일치하지 않으면 인증 실패로 응답합니다.
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.json({ success: false, message: 'Authentication failed' });
+    if(foundUser) {
+        req.session.user_id = foundUser._id;
+        res.json({ success: true, user: { username: foundUser.username } });
     }
 
-    // 인증이 성공하면 세션에 사용자 ID를 저장하고 클라이언트에게 성공과 사용자 정보를 응답합니다.
-    req.session.user_id = user._id;
-    res.json({ success: true, user: { username: user.username } });
 });
 
 // 사용자 로그인 확인 엔드포인트
